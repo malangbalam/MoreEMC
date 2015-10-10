958537568d345464e20243cb5ed87d9e61854fcd
hasunwoo <whs1999kr@naver.com>
2015-10-10 11:59:37 +0900

    added sidecontroller

--- src/main/java/mlbm/moreEMC/main/Constants.java
+++ src/main/java/mlbm/moreEMC/main/Constants.java
@@ -1,5 +1,8 @@
 package mlbm.moreEMC.main;
 
+/**
+ * @author hasunwoo do not implement this interface.
+ */
 public interface Constants {
 	public static final String MODID = "moreEMC";
 	public static final String VERSION = "0.1.1";
